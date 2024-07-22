4bf077b67c3077a73ea0d2d280755e5593e0c267
thakre49 <ritikthakre49@gmail.com>
2024-07-22 22:48:02 +0530

    Finished Backend

---
+++ constants/config.js
@@ -0,0 +1,16 @@
+
+const corsOptions = {
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:4173",
+        process.env.CLIENT_URL,
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+};
+
+const SANGAM_TOKEN = "sangam-token";
+
+export { corsOptions, SANGAM_TOKEN };
+
+
